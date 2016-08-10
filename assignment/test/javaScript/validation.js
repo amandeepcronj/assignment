59a7ea345317a5c36e7ba86fb24f0cce52fd730d
amandeep <amandeep@cronj.com>
2016-08-10 18:14:05 +0530

    assign1:employee addition

---
+++ assignment/test/javaScript/validation.js
@@ -0,0 +1,44 @@
+function valid()
+{
+	
+
+	var username=document.getElementById('username').value;
+	var password=document.getElementById('password').value;
+	if(username=="" || password=="")
+	{
+		alert('please do not leave field blank');
+	}
+	
+	else if(username=='amancronj' && password==12345)
+	{
+		alert("Correct login");
+		
+		window.open("hr.html");
+
+
+	}
+	else
+	{
+		alert("Wrong id password");
+	}
+
+	//document.getElementById('posts').innerHTML="hahahahaha";
+	return false;
+
+}
+
+function show()
+{
+
+		document.getElementById('emp_form1').style.display='block';
+}
+
+function data_inp()
+{
+	var user=document.getElementById('emp_name').value;
+	var pass=document.getElementById('emp_pass').value;
+
+	
+	return false;
+}
+
